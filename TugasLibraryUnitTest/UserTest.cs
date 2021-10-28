cc20762f8675e2b034b86aeadaaa3456fbce183c
ema-devani <vaniputri0603@gmail.com>
2021-10-28 23:43:20 +0700

    Unit Testing

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibrary
+{
+    [TestFixture]
+    public class UserTest
+    {
+        /// <summary>
+        /// Method ini akan mencocokan username dan password yang diberikan
+        /// dengan username dan password yang terdaftar dalam database.
+        /// </summary>
+        /// <param name="userName">Diisi dengan user name</param>
+        /// <param name="password">Diisi dengan password</param>
+        /// <returns>
+        /// Apabila cocok, akan menghasilkan true, dan apabila tidak cocok akan menghasilkan false.
+        /// </returns>
+       
+        [Test]
+        public void TestUserValid()
+        {
+            User user = new User();
+            string userName = "user name";
+            string password = "password";
+            bool expected = true;
+            bool actual;
+            actual = user.IsValidUser(userName, password);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4030695-324a-4dd5-96ca-407b37540267")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
